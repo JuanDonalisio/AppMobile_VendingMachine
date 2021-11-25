3894d8242d485d680baacf234e1d2db919c6a68a
Juan Pablo Donalisio <jpdona@hotmail.com>
2021-11-25 17:26:14 -0300

    main menu update

--- com_ricks_machine/lib/screens/main_menu_screen.dart
+++ com_ricks_machine/lib/screens/main_menu_screen.dart
@@ -14,11 +14,73 @@ class _MainScreenState extends State<MainScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/main_screen.jpeg'),
+            image: AssetImage('assets/main_screen.png'),
             fit: BoxFit.cover,
           )
         ),
+        child: Column(
+          children:<Widget>[
+            SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  child: Align(
+                    child: Container(
+                      child: Align(
+                        alignment: Alignment(0.1,-0.92) ,
+                        child: IconButton(
+                          onPressed: () {  },
+                          iconSize: 50,
+                          icon: Image.asset('assets/settings_button.png'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Align(
+                    child: Container(
+                      child: Align(
+                        alignment: Alignment(0.98,-0.92) ,
+                        child: IconButton(
+                          onPressed: () {  },
+                          iconSize: 50,
+                          icon: Image.asset('assets/close_button.png'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 100,),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {  },
+                    alignment: Alignment(6,-0.12),
+                    iconSize: 200,
+                    icon: Image.asset('assets/play_button.png'),
+                  ),
+                ],
+              )
+            ),
+            SizedBox(height: 150,),
+            Container(
+              child: Align(
+                alignment: Alignment(-0.01,-0.92) ,
+                child: IconButton(
+                  onPressed: () {  },
+                  iconSize: 200,
+                  icon: Image.asset('assets/play_button.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    );
+      );
   }
 }
